2014658141be3928898101fa0d609708e79d39e6
LinuX Man <linuxman.linux@gmail.com>
2020-03-15 00:03:45 +0530

    There's no changes were made

--- src/com/lxmindustries/Main.java
+++ src/com/lxmindustries/Main.java
@@ -3,12 +3,12 @@ package com.lxmindustries;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Shapes shapes = new Shapes();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Shapes shapes = new Shapes();
 
-        System.out.print("Input a limit to make a triangle => ");
-        shapes.triangle(sc.nextInt());
-        sc.close();
-    }
+		System.out.print("Input a limit to make a triangle => ");
+		shapes.triangle(sc.nextInt());
+		sc.close();
+	}
 }
