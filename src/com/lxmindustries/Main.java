af18345dc8e2ad2358cd2eac80d8b5dc627ae0bc
Dipak Bari <dipakbari4@gmail.com>
2020-03-15 19:29:25 +0530

    fixes casting issue

--- src/com/lxmindustries/Main.java
+++ src/com/lxmindustries/Main.java
@@ -3,12 +3,26 @@ package com.lxmindustries;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Shapes shapes = new Shapes();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Shapes shapes = new Shapes();
 
-		System.out.print("Input a limit to make a triangle => ");
-		shapes.triangle(sc.nextInt());
-		sc.close();
-	}
+        System.out.print("Input a limit to make a triangle => ");
+        String inputString = sc.next();        // Getting data as string from console
+
+        /*
+         * The input might be other than number should have safe
+         * casting handler. Otherwise, it'll throw exceptions.
+         */
+        try {
+            int inputNumber = Integer.parseInt(inputString);    // Parsing to integer
+            shapes.triangle(inputNumber);                        // Calling the object's method and passing converted value
+        } catch (Exception ex) {        // Catching exception for other characters
+            ex.printStackTrace();    // remove this line
+            System.out.println("Please enter a number");
+        } finally {
+            sc.close();                                            // finally close the scanner
+        }
+
+    }
 }
